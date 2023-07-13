6d0d4c386592e72f43a97c80588cf552d8316af4
RidhikaJoshi <ridhikasmrh@gmail.com>
2023-07-13 09:36:22 +0530

    LinkedList

---
+++ Linked List/Count_nodes_of_linked_list.cpp
@@ -0,0 +1,26 @@
+/*
+Given a singly linked list. The task is to find the length of the linked list, where length is defined as the number of nodes in the linked list.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    public:
+    //Function to count nodes of a linked list.
+    int getCount(struct Node* head)
+    {
+        int count=0;
+        Node *temp=head;
+        while(temp!=NULL)
+        
+        {
+            count++;
+            temp=temp->next;
+        }
+    
+        return count;
+    
+    }
+};

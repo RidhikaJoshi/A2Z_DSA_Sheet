33e58ee9bf81ddbfd0a726367a9b599ca36777fe
RidhikaJoshi <ridhikasmrh@gmail.com>
2023-07-31 15:28:12 +0530

    Questions

--- Arrays/Move_all_zeroes_to_end_of_array.cpp
+++ Arrays/Move_all_zeroes_to_end_of_array.cpp
@@ -27,3 +27,31 @@ public:
 	}
 };
 //Optimised Solution
+
+//Intuition: first find the 1st zero int the array and stroe in j
+// then iterate through j+1 till the end 
+// every time a non-zero number is encountered then we swap elemts at index i and j 
+// increase the j pointer by 1
+
+class Solution{
+public:
+	void pushZerosToEnd(int arr[], int n)
+	{
+	    int j;
+	    for(j=0;j<n;j++)
+	    {
+	        if(arr[j]==0)
+	            break;
+	    }
+	    for(int i=j+1;i<n;i++)
+	    {
+	        if(arr[i]!=0)
+	        {
+	            swap(arr[i],arr[j]);
+	            j++;
+	        }
+	       
+	    }
+
+	}
+};

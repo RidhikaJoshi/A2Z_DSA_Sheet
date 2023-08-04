e2361bfc4782a50582147b9dc32d3dd71ecfc170
RidhikaJoshi <ridhikasmrh@gmail.com>
2023-08-04 20:06:28 +0530

    Questions

---
+++ Stack and Queue/Reverse_a_Stack.cpp
@@ -0,0 +1,25 @@
+/*
+You are given a stack St. You have to reverse the stack using recursion.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution{
+public:
+    stack<int>s;
+    void Reverse(stack<int> &St)
+    {
+        if(St.size()==0)
+         {
+             St=s;
+             return;
+         }
+            
+        int x=St.top();
+        St.pop();
+        s.push(x);
+        Reverse(St);
+        
+    }
+};

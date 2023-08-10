ca161b6be1a2563b4015374ad3102ee6f896afe9
RidhikaJoshi <ridhikasmrh@gmail.com>
2023-08-11 02:08:50 +0530

    Questions

--- Binary_Search/Implement_Upper_Bound.cpp
+++ Binary_Search/Implement_Upper_Bound.cpp
@@ -7,7 +7,23 @@ But if any such index is not found, the upper bound algorithm returns n i.e. siz
 */
 #include<bits/stdc++.h>
 using namespace std;
-
+// Striver optimal Solution
+int upperBound(vector<int> &arr, int x, int n)
+{
+	int low=0,high=n-1,ans=n;
+	while(low<=high)
+	{
+		int mid=low+(high-low)/2;
+                if (arr[mid] > x) {
+                  ans = mid;
+                  high = mid - 1;
+                }
+				else
+					low=mid+1;
+        }	
+		return ans;
+}
+// My Solution
 int upperBound(vector<int> &arr, int x, int n)
 {
 	int low=0,high=n-1,mid;

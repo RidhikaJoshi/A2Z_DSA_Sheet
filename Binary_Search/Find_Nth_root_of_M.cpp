45d8333726f315d1d39e00ff96dbc705229376af
RidhikaJoshi <ridhikasmrh@gmail.com>
2023-08-12 12:14:39 +0530

    Questions

--- Binary_Search/Find_Nth_root_of_M.cpp
+++ Binary_Search/Find_Nth_root_of_M.cpp
@@ -8,15 +8,33 @@ using namespace std;
 
 class Solution{
 	public:
+	// to calculate the power function
+	// overflow case might occur
+	// so this function returns 1 if power is equal else 2 if greater else 0 if smaller
+	int power(int mid,int n,int m)
+	{
+		long long ans=1;
+		for(int i=1;i<=n;i++)
+		{
+			ans=ans*mid;
+			if(ans>m)
+				return 2;
+		}
+		if(ans==m)
+			return 1;
+		else		
+			return 0;
+
+	}
 	int NthRoot(int n, int m)
 	{
 	   int low=1,high=m;
 	   while(low<=high)
 	   {
 	       int mid=low+(high-low)/2;
-	       if(pow(mid,n)==m)
+	       if(power(mid,n,m)==1)
 	        return mid;
-	       else if(pow(mid,n)>m)
+	       else if(power(mid,n,m)==2)
 	        high=mid-1;
 	       else
 	        low=mid+1;

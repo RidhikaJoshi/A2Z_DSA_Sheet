4158592cd0eec43dfe6dc6fd283379e7d3da53c4
RidhikaJoshi <ridhikasmrh@gmail.com>
2023-08-27 20:58:37 +0530

    Questions

--- Arrays/Rotate_Array.cpp
+++ Arrays/Rotate_Array.cpp
@@ -1,28 +1,29 @@
 /*
-Given an unsorted array arr[] of size N. Rotate the array to the left (counter-clockwise direction) by D steps, where D is a positive integer. 
+Given an unsorted array arr[] of size N. Rotate the array to the left (counter-clockwise direction) by D steps, where D is a positive integer.
 */
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+class Solution
+{
 
-class Solution{
-    public:
-    void rotate(int arr[],int low,int high)
+public:
+    void rotate(int arr[], int low, int high)
     {
-        while(low<=high)
+        while (low <= high)
         {
-            swap(arr[low],arr[high]);
+            swap(arr[low], arr[high]);
             low++;
             high--;
         }
     }
-    //Function to rotate an array by d elements in counter-clockwise direction. 
+    // Time-O(N)
+    // Function to rotate an array by d elements in counter-clockwise direction.
     void rotateArr(int arr[], int d, int n)
     {
-        d=d%n;
-        rotate(arr,0,d-1);
-        rotate(arr,d,n-1);
-        rotate(arr,0,n-1);
-        
+        d = d % n;
+        rotate(arr, 0, d - 1);
+        rotate(arr, d, n - 1);
+        rotate(arr, 0, n - 1);
     }
 };

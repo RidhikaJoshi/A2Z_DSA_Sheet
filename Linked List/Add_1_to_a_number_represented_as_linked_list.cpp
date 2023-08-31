45b8ed0f884ce6753af1a266e8eb35ebe0d05373
RidhikaJoshi <ridhikasmrh@gmail.com>
2023-08-31 10:35:30 +0530

    Questions

--- Linked List/Add_1_to_a_number_represented_as_linked_list.cpp
+++ Linked List/Add_1_to_a_number_represented_as_linked_list.cpp
@@ -2,7 +2,7 @@
 A number N is represented in Linked List such that each digit corresponds to a node in linked list. You need to add 1 to it.
 */
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 struct Node
@@ -11,64 +11,63 @@ struct Node
     Node *next;
     Node(int x)
     {
-        val=x;
-        next=NULL;
+        val = x;
+        next = NULL;
     }
-
 };
-
 
 class Solution
 {
-    public:
+public:
+    //
+    //
+    // Time=O(N)
     Node *reverse(Node *head)
     {
-        Node *curr=head,*prev=NULL;
-        while(curr!=NULL)
+        Node *curr = head, *prev = NULL;
+        while (curr != NULL)
         {
-            Node *temp=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=temp;
+            Node *temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
         }
         return prev;
     }
 
-    
-    Node* addOne(Node *head) 
+    // Time=O(N)
+    Node *addOne(Node *head)
     {
-        Node *newhead=reverse(head);
-        int c=1;
-        Node *temp=newhead;
-        Node *head1=NULL,*t=NULL;
-        while(temp!=NULL)
+        Node *newhead = reverse(head);
+        int c = 1;
+        Node *temp = newhead;
+        Node *head1 = NULL, *t = NULL;
+        while (temp != NULL)
         {
-            int x=temp->data+c;
-            c=x/10;
-            x=x%10;
-           
-            Node *curr=new Node(x);
-            if(head1==NULL)
+            int x = temp->data + c;
+            c = x / 10;
+            x = x % 10;
+
+            Node *curr = new Node(x);
+            if (head1 == NULL)
             {
-                head1=curr;
-                t=curr;
-                
+                head1 = curr;
+                t = curr;
             }
             else
             {
-                t->next=curr;
-                t=t->next;
+                t->next = curr;
+                t = t->next;
             }
-            
-            temp=temp->next;
-            
+
+            temp = temp->next;
         }
-        if(c!=0)
+        if (c != 0)
         {
-            Node *curr=new Node(c);
-            t->next=curr;
+            Node *curr = new Node(c);
+            t->next = curr;
         }
-        head1=reverse(head1);
+        head1 = reverse(head1);
         return head1;
     }
 };

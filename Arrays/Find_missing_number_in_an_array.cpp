0c975ef9df47ced3622b7c79124bdf289c320246
RidhikaJoshi <ridhikasmrh@gmail.com>
2023-09-14 23:35:08 +0530

    Optimised missing number
    Signed-off-by: RidhikaJoshi <ridhikasmrh@gmail.com>

--- Arrays/Find_missing_number_in_an_array.cpp
+++ Arrays/Find_missing_number_in_an_array.cpp
@@ -1,22 +1,42 @@
 /*
 Given an array of size N-1 such that it only contains distinct integers in the range of 1 to N. Find the missing element.
 */
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution{
-  public:
-    int missingNumber(vector<int>& array, int n)
+class Solution
+{
+public:
+    // Time=O(n) & Space=O(n)
+    int missingNumber(vector<int> &array, int n)
     {
-        unordered_set<int>st;
-        for(int i=0;i<array.size();i++)
+        unordered_set<int> st;
+        for (int i = 0; i < array.size(); i++)
             st.insert(array[i]);
-            
-        for(int i=1;i<=n;i++)
+
+        for (int i = 1; i <= n; i++)
         {
-            if(st.find(i)==st.end())    
+            if (st.find(i) == st.end())
                 return i;
         }
         return -1;
+    }
+};
+// Time=O(n) & Space=O(1)
+class Solution
+{
+public:
+    int missingNumber(vector<int> &array, int n)
+    {
+        int res = 0;
+        for (int i = 0; i < array.size(); i++)
+            res = res ^ array[i];
+
+        int result = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            result = result ^ i;
+        }
+        return res ^ result;
     }
 };
